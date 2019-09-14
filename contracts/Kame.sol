099c802783888ee02de0ffb1f4c3ff8cdd466fc7
Bradley C <bradleyctclarke@gmail.com>
2019-09-13 21:34:44 -0700

    add contract methods

--- contracts/Kame.sol
+++ contracts/Kame.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.5.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract Kame is ERC721Full {
+contract Kame is ERC721Full, ERC721Mintable, Ownable {
   constructor (string memory name, string memory symbol) public ERC721Full(name, symbol) {
       // solhint-disable-previous-line no-empty-blocks
     }
@@ -13,10 +15,39 @@ contract Kame is ERC721Full {
     bool mainnetLock;
   }
 
-  // mint
+  Kora[] public kora;
+
+  // TODO: check that each of these should be in the OpenZeppelin contracts
   // burn
   // register admin
   // register minter
   // buy
   // transfer
+
+  function mintKora (bytes32 metadata) public returns(bool, uint256) {
+    address minter = msg.sender;
+    uint256 newTokenId = kora.length++;
+    Kora memory newKora = Kora({
+      metadata: metadata,
+      minter: minter,
+      mainnetLock: false
+    });
+    if (mint(minter, newTokenId) == true) {
+      kora.push(newKora);
+      return (true, newTokenId);
+    } else return (false, 0);
+  }
+
+  function getTokenById (uint256 id) public view returns(bytes32, address, bool) {
+    Kora memory k = kora[id];
+    return(k.metadata, k.minter, k.mainnetLock);
+  }
+
+  function lockToMainnet (uint256 id) public onlyOwner returns(bool) {
+    if (kora[id].mainnetLock = true) {
+      return true;
+    } else return false;
+  }
+
+  function () external payable {}
 }
