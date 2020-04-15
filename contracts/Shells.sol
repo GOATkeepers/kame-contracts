5d764008ed1b1b98ebf4e5b577cd950d27140f6e
Bradley Clarke <bradleyctclarke@gmail.com>
2020-04-14 23:58:30 -0700

    Update Shells.sol
    make total supply dynamic upon deployment

--- contracts/Shells.sol
+++ contracts/Shells.sol
@@ -6,14 +6,10 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract Shells is ERC20, ERC20Detailed, ERC20Burnable, ERC20Pausable {
-  uint public INITIAL_SUPPLY = 1000000;  // If you want this dynamic from truffle migrations move it to constructor
-
-  constructor(string memory _name, string memory _symbol)
+  constructor(string memory _name, string memory _symbol, uint memory _supply)
     // (name, symbol, decimal places)
     // (looked weird in wallets when we use decimal.))
     ERC20Detailed(_name, _symbol, 0) public {
-    _mint(msg.sender, INITIAL_SUPPLY);
+    _mint(msg.sender, _supply);
   }
 }
-
-// Seems that "_mint" will not be available after initial deploy, so if we want it to, add here.
